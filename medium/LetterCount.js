84b240d32f79cd8c21ee36bf9f312f906c97112d
Kyle Cho <kylecho.work@gmail.com>
2015-07-04 20:17:45 -0700

    Add LetterCount.js

---
+++ medium/LetterCount.js
@@ -0,0 +1,35 @@
+function LetterCount(str) {
+
+  var compare = function(a, b) {
+      if (a < b) {
+          return -1;
+      } else if (a > b) {
+          return 1;
+      } else {
+          return 0;
+      }
+  };
+  
+  var copy = str.split(' ').slice();
+  var myArr = copy.slice();
+  var max = 0;
+  var count = 0;
+  var theIndex;
+  var word;
+
+  for (var i = 0; i < myArr.length; i++) {
+    word = myArr[i].split('').sort(compare);
+    for (var j = 0; j < word.length - 1; j++) {
+      if (word[j] === word[j + 1]) {
+        count++;
+      }
+    }
+    if (count > max) {
+      max = count;
+      theIndex = i;
+      count = 0;
+    }
+  }
+  
+  return (theIndex !== undefined) ? copy[theIndex] : -1;
+}
